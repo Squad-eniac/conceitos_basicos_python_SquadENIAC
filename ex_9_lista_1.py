73add651cdeaf8bc1a4fe206ec6fbe1fed5b2a02
ignacioFabiana <faby.tbe@gmail.com>
2024-08-02 14:55:30 -0300

    chore: delete obsolete README and ex_9_lista_1.py

--- ex_9_lista_1.py
+++
@@ -1,9 +0,0 @@
-''' Solicite ao usuário o número de horas de exercício físico por semana.
-Calcule o total de calorias queimadas em um mês, considerando uma média de 5 calorias por minuto de exercício'''
-
-def calculate_calories_burned():
-    activity_hours_per_week = float(input('Insira o número de horas de atividade física \n que você faz por semana: '))
-    calories_burned_per_week = (activity_hours_per_week * 60)*5
-    print (f'Você queimou {(calories_burned_per_week * 4):.2f} calorias esse mês')
-
-calculate_calories_burned()    
